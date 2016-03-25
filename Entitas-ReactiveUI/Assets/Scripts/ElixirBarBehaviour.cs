4b369138ccc05fb43b108e19b8c77f196ef1a445
Maxim Zaks <maxim.zaks@gmail.com>
2016-03-25 18:51:16 +0100

    making elixir bar implementation simpler and work with WebGL

--- Entitas-ReactiveUI/Assets/Scripts/ElixirBarBehaviour.cs
+++ Entitas-ReactiveUI/Assets/Scripts/ElixirBarBehaviour.cs
@@ -2,17 +2,14 @@
 
 public class ElixirBarBehaviour : MonoBehaviour, IElixirListener {
 
-	float maxWidth;
-
 	void Start () {
-		maxWidth = GetComponent<RectTransform>().rect.width;
 		Pools.pool.CreateEntity().AddElixirListener(this);
 	}
 
 	public void ElixirAmountChanged ()
 	{
 		var ratio = Pools.pool.elixir.amount / ElixirProduceSystem.ElixirCapacity;
-		GetComponent<RectTransform>().sizeDelta = new Vector2(maxWidth * ratio, GetComponent<RectTransform>().rect.height);
+		GetComponent<RectTransform>().localScale = new Vector3(ratio, 1f, 1f);
 	}
 
 }
